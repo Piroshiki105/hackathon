6620eaccd0b991575b6a7a7a97e92b8a4862adc7
arata wada <wada.arata10@gmail.com>
2018-12-08 17:15:02 +0900

    mapをconfirmでもつかう

--- app/controllers/confirm_controller.rb
+++ app/controllers/confirm_controller.rb
@@ -4,6 +4,9 @@ class ConfirmController < ApplicationController
 
     def confirm
         if params['blocks'] != nil && params['blocks'].length > 0  then
+
+            maze = Maze.new params['stage']  
+            @maze = maze.getMaze
             setBlockDisassembly params['blocks']
             @blockArray = getblockArray
             initFunction getblockArray
@@ -47,6 +50,27 @@ class ConfirmController < ApplicationController
             end
             @function = function
         end
+    end
+
+end
+
+class Maze
+    @maze
+
+    def initialize stage
+        case stage
+        when "44"
+            @maze = 'var maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]];'
+        when "55"
+            @maze = 'var maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]];'
+        when "66"
+            @maze = 'var maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]];'
+        else
+        end
+    end
+
+    def getMaze
+        return @maze
     end
 
 end
