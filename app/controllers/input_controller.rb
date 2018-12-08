d152760a19010d19d56da05108c045a27dc28a88
arata wada <wada.arata10@gmail.com>
2018-12-08 15:44:26 +0900

    配列出力修正

--- app/controllers/input_controller.rb
+++ app/controllers/input_controller.rb
@@ -1,42 +1,27 @@
 class InputController < ApplicationController
     def input
-        @stage = params['stage']
         maze = Maze.new params['stage']  
         @maze = maze.getMaze
     end
 end
 
 class Maze
-    @stage
-    @obstacle
-    @startPosition
-    @goalPosition
     @maze
 
     def initialize stage
-        @stage = stage
-        @startPosition
-        @endPosition
-    end
-
-    def initialize stage
         case stage
-        when 44
-            @maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]]
-        when 55
-            @maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]]
-        when 66
-            @maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]]
+        when "44"
+            @maze = 'var maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]];'
+        when "55"
+            @maze = 'var maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]];'
+        when "66"
+            @maze = 'var maze = [[-1, -1, -1, -1, -1, -1], [-1, 1, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 0, -1], [-1, 0, 0, 0, 2, -1], [-1, -1, -1, -1, -1, -1]];'
         else
         end
     end
 
     def getMaze
         return @maze
-    end
-
-    def createObstacle
-        rand(@n) + 1
     end
 
 end
